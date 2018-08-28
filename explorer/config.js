1cd01576f021fb1d29edcf359bbcf4535f15b856
Yuming He <yuming.p.he@gmail.com>
2018-08-27 23:44:40 -0400

    Update block explorer

--- explorer/config.js
+++ explorer/config.js
@@ -1,13 +1,20 @@
-'use strict'
+var web3 = require('web3');
+var net = require('net');
 
-const vars = [
-    'JSONRPC_HOST',
-    'JSONRPC_PORT'
-]
-
-const config = {
-    'JSONRPC_HOST': 'public-node',
-    'JSONRPC_PORT': '8545'
+var RPC = {
+  'host': 'public-node',
+  'port': '8545'
 }
 
-module.exports = config
+var config = function () {
+  this.logFormat = "combined";
+  this.provider = new web3.providers.HttpProvider('http://' + RPC.host + ':' + RPC.port);
+  this.bootstrapUrl = "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/yeti/bootstrap.min.css";
+
+  this.names = {
+    "Terra-Chain": "TODO"
+  }
+
+}
+
+module.exports = config;
